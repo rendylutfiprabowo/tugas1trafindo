94e3abbe028193d25e2f1bfe62778023dc235e05
MRafliAditya <rafli14aditya@gmail.com>
2023-08-24 21:16:22 +0700

    Migration

---
+++ app/Database/Migrations/2023-08-24-135748_Product.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Product extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 10,
+                'auto_increment' => true
+            ],
+            'nama_product' => [
+                'type' => 'VARCHAR',
+                'constraint' => '225',
+            ],
+            'desc_product' => [
+                'type' => 'VARCHAR',
+                'constraint' => '225'
+            ],
+            'id_user' => [
+                'type' => 'INT',
+                'constraint' =>10,
+                
+            ],
+            // ... add other columns
+        ]);
+          
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('product');
+    }
+    
+    public function down()
+    {
+        $this->forge->dropTable('product');
+    }
+}

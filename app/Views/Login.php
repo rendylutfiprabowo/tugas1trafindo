a159152259767734a996d5e1a0f2a8ef615f4c61
edolaksan <95566921+edolaksan@users.noreply.github.com>
2023-08-27 10:24:35 +0700

    view

--- app/Views/Login.php
+++ app/Views/Login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>Snippet - GoSNippets</title>
+    <title>Task Tracker PT Trafoindo Prima Perkasa</title>
     <link rel="stylesheet" href="../css/login.css">
     <link href='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css' rel='stylesheet'>
     <link href='https://use.fontawesome.com/releases/v5.7.2/css/all.css' rel='stylesheet'>

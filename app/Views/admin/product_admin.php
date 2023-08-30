add77efc3d6d2f0f9f713853f40a55d7f808fe82
calvinnickholaa <nickholascalvin1@gmail.com>
2023-08-30 20:44:15 +0700

    button close add modal

--- app/Views/admin/product_admin.php
+++ app/Views/admin/product_admin.php
@@ -108,7 +108,7 @@
           </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
           <button type="submit" class="btn btn-primary">Save</button>
         </div>
       </form>
